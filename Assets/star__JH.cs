2efaf3466daa905a61eb8f25da98830dd2dd86c0
fsmxv <fsmxvtjdgh740508@gmail.com>
2025-09-04 20:28:40 +0900

    kjh20250904

---
+++ Assets/star__JH.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class star_JH : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
